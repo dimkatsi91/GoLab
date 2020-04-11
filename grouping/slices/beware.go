0590fa6151d77c7f215968dfdf4517a0a3163986
Dimos Katsimardos <dimoskatsimardos@protonmail.com>
2020-04-11 17:29:35 +0300

    Golang's behavior in contrast to other langs ..

---
+++ grouping/slices/beware.go
@@ -0,0 +1,46 @@
+package main
+
+
+import "fmt"
+
+
+func print(slice []string) {
+	fmt.Println("#################################")
+	for _, player := range(slice) {
+		fmt.Println(player)
+	}
+	fmt.Println("#################################")
+}
+
+// CAUTION :: Errors will Occur for sure at first !
+//
+func main() {
+	// Names slice of strings ...
+	//
+	brazilians := []string{"Rivaldo", "Giovanni", "Ze Elias", "Luciano", "Alvez"}
+	slice := brazilians[2:5]
+	print(brazilians)
+	fmt.Println("")
+	slice[0] = "Karembue -- Ooops -- Cahnged ???"
+	print(brazilians)
+}
+
+/* Sample Output ::
+
+	#################################
+	Rivaldo
+	Giovanni
+	Ze Elias
+	Luciano
+	Alvez
+	#################################
+
+	#################################
+	Rivaldo
+	Giovanni
+	Karembue -- Ooops -- Cahnged ???
+	Luciano
+	Alvez
+	#################################
+
+*/
